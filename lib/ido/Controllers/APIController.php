aa5ee082c5a7deb087c2e003268e4803d3790b71
Frederick Lowe <me@fredericklowe.com>
2024-08-27 14:11:13 -0400

    Initial checkin

---
+++ lib/ido/Controllers/APIController.php
@@ -0,0 +1,15 @@
+<?php
+namespace Ido;
+
+class APIController extends \Ido\Base\Controller { 
+	public function __construct () 
+	{
+
+	}
+
+	public function run() : void 
+	{
+		echo 'APIController::run' . PHP_EOL;
+	}
+}
+?>
---
+++ lib/ido/Controllers/WebController.php
@@ -0,0 +1,15 @@
+<?php
+namespace Ido\Controllers;
+
+class WebController extends \Ido\Base\Controller { 
+	public function __construct () 
+	{
+
+	}
+
+	public function run() : void 
+	{
+		echo 'WebController::run' . PHP_EOL;
+	}
+}
+?>
